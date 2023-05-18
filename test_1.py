c0ed4613afc74b98c21392ebd7183d7b89f99f32
bavaaram <itsjustabavaar@gmail.com>
2023-05-18 13:22:30 +0330

    Add a section called Simple Calculator to test_1.py file

--- test_1.py
+++ test_1.py
@@ -2,3 +2,10 @@
 
 # Section 1: this section only print hello world!
 print("Hello World!")
+
+# section 2: Add a Simple Calculator
+def calc(num1, num2):
+    print(f"Summation is {int(num1) + int(num2)}")
+    print(f"Multiply is {int(num1) + int(num2)}")
+    print("Substraction is {int(num1) - int(num2)}")
+    print(f"Division is {int(num1) / int(num2)}")
